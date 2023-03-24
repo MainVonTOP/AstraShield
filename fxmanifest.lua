c7bb9cfe5f32fd7bcfdc6ab0a074fab1280348b8
MainVonTOP <111784551+MainVonTOP@users.noreply.github.com>
2023-03-24 13:33:05 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+author 'AstraShield - Anticheat'
+fx_version 'adamant'
+games {'gta5'}
+
+
+client_scripts {
+    'client/client.lua',
+    'config/*.lua',
+}
+
+server_scripts {
+    "server/server.lua",
+    'config/*.lua',
+    "server/server2.lua",
+}
+
